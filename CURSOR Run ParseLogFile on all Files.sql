601bba9299d79234e0979b5ddd99068990ecd49c
Chris Odegard <c.odegard@gmail.com>
2021-01-19 13:42:14 -0500

    Refine script

--- CURSOR Run ParseLogFile on all Files.sql
+++ CURSOR Run ParseLogFile on all Files.sql
@@ -1,95 +1,116 @@
 USE LabLog;
 GO
 
--- VARIABLES
-DECLARE @limit INT;
-DECLARE @counter INT;
-DECLARE @SourceFile varchar(50);
-DECLARE @Total INT;
+SET NOCOUNT ON;
+GO
 
-SET @limit = 10;
-SET @counter = 0;
+
+-- VARIABLES
+DECLARE @Limit INT;
+DECLARE @Counter INT;
+DECLARE @SourceFile varchar(50);
+DECLARE @TotalStaging INT;
+DECLARE @TotalProd INT;
+DECLARE @HistoryTimeStamp DATETIME;
+DECLARE @RunningTotal INT;
+
+SET @Limit = 20;
+SET @Counter = 0;
+SET @RunningTotal = 0;
 
 -- DECLARE the cursor as a SELECT statement
 DECLARE cursor_LogFiles CURSOR
-FOR SELECT TOP (@limit)
-		[SourceFile]
-		, COUNT([Id]) as [Total]
-	FROM [dbo].[Staging]
-	GROUP BY [SourceFile]
-	ORDER BY SUBSTRING([SourceFile], 5, 10), SUBSTRING([SourceFile], 0, 4) DESC
+FOR SELECT TOP (@Limit)
+		[SourceFile], [Total]
+	FROM [dbo].[FileCount_Staging]
+	ORDER BY SUBSTRING([SourceFile], 5, 10), SUBSTRING([SourceFile], 0, 4) DESC;
 
 -- OPEN the cursor for reading
-OPEN cursor_LogFiles
+OPEN cursor_LogFiles;
 
 -- (Try to) get the first result from the cursor
-FETCH NEXT FROM cursor_LogFiles INTO @SourceFile, @Total;
+FETCH NEXT FROM cursor_LogFiles INTO @SourceFile, @TotalStaging;
 
 -- Loop through the cursor results
 -- @@FETCH_STATUS is 0 when the previous FETCH returned something
 WHILE @@FETCH_STATUS = 0
     BEGIN
-    FETCH NEXT FROM cursor_LogFiles INTO @SourceFile, @Total;
+		FETCH NEXT FROM cursor_LogFiles INTO @SourceFile, @TotalStaging;
 
-    PRINT CAST(@counter AS VARCHAR) + CHAR(9) + @SourceFile + CHAR(9) + CAST(@Total AS VARCHAR);
+		PRINT (CAST(@Counter AS VARCHAR) + CHAR(9) + @SourceFile + CHAR(9) + CAST(@TotalStaging AS VARCHAR));
 
-    -- Delete existing data
-    DELETE FROM dbo.[ParsedFieldValues] WHERE [SourceFile] = @SourceFile
+		-- Check if existing number of rows is the same in PROD/STAGING for the @Sourcefile
+		SELECT @TotalProd = (SELECT [Total] FROM dbo.[FileCount] WHERE [SourceFile] = @SourceFile);
+		-- SELECT @TotalStaging = (SELECT [Total] FROM dbo.[FileCount_Staging] WHERE [SourceFile] = @SourceFile);
+		-- PRINT @TotalProd; PRINT @TotalStaging;
 
-	
-    -- Insert new data
-	BEGIN TRY
-		BEGIN TRANSACTION
-			INSERT INTO dbo.[ParsedFieldValues]
-			SELECT
-			A.[Id]
-			, A.[SourceFile]
-			, A.[Level]
-			, C.Pos3 AS [Category]
-			, C.Pos5 AS [TimeStamp]
-			-- , null as [DateStamp]
-			, C.Pos6 AS [EventID]
-			, C.Pos7 AS [Windows Log]
-			, C.Pos10 AS [Short Message]
-			, C.Pos11 AS [Host Name]
-			, C.Pos13 AS [Message Subject]
-			FROM dbo.[Staging] A
-			Cross Apply [dbo].[udf-Str-Parse](A.[Message], char(13)) B
-			Cross Apply [dbo].[udf-Str-Parse-Row](B.[RetVal], char(9)) C
-			WHERE B.[RetVal] is not null 
-				AND B.[RetSeq] = 1
-				AND A.[SourceFile] = @SourceFile
-			ORDER BY A.[Id]
-		COMMIT TRANSACTION
-	END TRY
-	BEGIN CATCH
-		INSERT INTO dbo.[DB_Errors]
-		VALUES
-		(
-			SUSER_SNAME(),
-			ERROR_NUMBER(),
-			ERROR_STATE(),
-			ERROR_SEVERITY(),
-			ERROR_LINE(),
-			ERROR_PROCEDURE(),
-			ERROR_MESSAGE(),
-			GETDATE()
-		);
+		IF (@TotalProd = @TotalStaging)
+			PRINT (CHAR(9) + 'ALREADY IMPORTED. SKIPPING.');
 		
-		-- Transaction uncommittable
-		IF (XACT_STATE()) = -1
-			ROLLBACK TRANSACTION
-			PRINT 'Rollback'
- 
-		-- Transaction committable
-		IF (XACT_STATE()) = 1
-			COMMIT TRANSACTION
-			PRINT 'Commit'
-	END CATCH
+		ELSE 
+			-- Insert new data
+			BEGIN TRY
+				BEGIN TRANSACTION
+					DELETE FROM dbo.[ParsedFieldValues] WHERE [SourceFile] = @SourceFile;
+					-- Log to History 
+					-- SET @HistoryTimeStamp = CURRENT_TIMESTAMP;
+					-- EXECUTE [dbo].[AddToHistory] @HistoryTimeStamp, 'Delete', @TotalProd, 'ParsedFieldValues', @SourceFile;
 
-    -- Increment the counter
-    SET @counter = @counter + 1;
+					INSERT INTO dbo.[ParsedFieldValues]
+					SELECT
+					[Id]
+					, [SourceFile]
+					, [Level]
+					, [Category]
+					, [TimeStamp]
+					-- [DateStamp]
+					, [EventID]
+					, [Windows Log]
+					, [Short Message]
+					, [Host Name]
+					, [Message Subject]
+					FROM [dbo].[ParseFields]
+					WHERE [SourceFile] = @SourceFile
+					ORDER BY [Id];
+				COMMIT TRANSACTION
+				SET @RunningTotal = @RunningTotal + @TotalStaging;
+				PRINT (CHAR(9) + 'INSERTED ' + CONVERT(VARCHAR, @TotalStaging) + ' ROWS');
+
+				-- Log to History 
+				-- SET @HistoryTimeStamp = CURRENT_TIMESTAMP;
+				-- EXECUTE [dbo].[AddToHistory] @HistoryTimeStamp, 'Insert', @TotalStaging, 'ParsedFieldValues', @SourceFile;
+			END TRY
+			BEGIN CATCH
+				INSERT INTO dbo.[DB_Errors]
+				VALUES
+				(
+					SUSER_SNAME(),
+					ERROR_NUMBER(),
+					ERROR_STATE(),
+					ERROR_SEVERITY(),
+					ERROR_LINE(),
+					ERROR_PROCEDURE(),
+					ERROR_MESSAGE(),
+					GETDATE()
+				);
+				
+				-- Transaction uncommittable
+				IF (XACT_STATE()) = -1
+					ROLLBACK TRANSACTION
+					PRINT ('ERROR! ROLLBACK OVERWRITE FOR ' + @SourceFile);
+		
+				-- Transaction committable
+				IF (XACT_STATE()) = 1
+					COMMIT TRANSACTION
+					PRINT ('SUCCESS. COMMIT OVERWRITE FOR ' + @SourceFile);
+			END CATCH
+		;
+
+		-- Increment the counter
+		SET @Counter = @Counter + 1;
 END;
+
+PRINT ('INSERTED ' + CONVERT(VARCHAR, @RunningTotal) + ' TOTAL ROWS');
 
 -- CLOSE and DEALLOCATE the cursor to clean up
 CLOSE cursor_LogFiles;
