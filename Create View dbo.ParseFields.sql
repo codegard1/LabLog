7a184db4e0f8d901cb5f27886e09ebf0837b976d
Chris Odegard <c.odegard@ramsa.com>
2021-01-29 14:44:57 -0500

    Update View to parse Message content better

--- Create View dbo.ParseFields.sql
+++ Create View dbo.ParseFields.sql
@@ -3,25 +3,34 @@ GO
 SET QUOTED_IDENTIFIER ON
 GO
 
-CREATE VIEW [dbo].[ParseFields]
+ALTER VIEW [dbo].[ParseFields]
 AS
 
-Select
+Select 
+	--TOP 100
     A.Id
     , A.SourceFile
     , A.Level
     , CAST(A.[Timestamp] AS DATE) AS Datestamp
+	, A.TimeStamp
+
     -- ,B.RetSeq
     -- ,B.RetVal
     -- , B.*
-    -- , C.Pos6 AS 'Level'
+    --, C.Pos6 AS 'Level'
+
     , C.Pos3 AS 'Category'
-    , C.Pos5 AS 'TimeStamp'
+    --, C.Pos5 AS 'TimeStamp'
     , C.Pos6 AS 'EventID'
     , C.Pos7 AS 'Windows Log'
+	, C.Pos11 AS 'Host Name'
     , C.Pos10 AS 'Short Message'
-    , C.Pos11 AS 'Host Name'
-    , C.Pos13 AS 'Message Subject'
+    --, C.Pos13 AS 'Message Subject'
+	, [Message Body] = LTRIM(RTRIM(
+				SUBSTRING( A.[Message], 
+				PATINDEX('%' + C.Pos13 + '%', A.[Message]), 
+				LEN(A.Message)-PATINDEX('%' + C.Pos13 + '%', A.[Message])+1)
+			))
 From dbo.Staging A
  Cross Apply [dbo].[udf-Str-Parse](A.Message,char(13)) B
  Cross Apply [dbo].[udf-Str-Parse-Row](B.RetVal,char(9)) C
